ee2a06f2225ca2bc580a0d93d2c0df50ca1ec514
alindsingh281 <59726689+alindsingh281@users.noreply.github.com>
2020-01-17 19:40:58 +0530

    Add files via upload

--- BinSearch.cpp
+++ BinSearch.cpp
@@ -1,20 +1,62 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 bool binarySearchReacursive(int* array, int startIndex, int lastIndex, int element)
 {
-  return true;
+  if(startIndex>lastIndex)
+      return false;
+  else
+  {
+      int mid=(startIndex+lastIndex)/2;
+      if(array[mid]==element)
+          return true;
+      else if(array[mid]>element)
+          binarySearchReacursive(array, startIndex, mid-1, element);
+      else
+          binarySearchReacursive(array, mid+1, lastIndex, element);
+  }
 }
 
 bool binarySearchIterative(int* array, int startIndex, int lastIndex, int element)
 {
-  return true;
+  while(startIndex<=lastIndex)
+  {
+    int mid = (startIndex+lastIndex)/2;
+    if(array[mid] == element)
+        return true;
+    else if(array[mid] < element)
+        startIndex = mid+1;
+    else if(array[mid] > element)
+        lastIndex = mid-1;
+   }
+   if(startIndex>lastIndex)
+        return false;
 }
 
 int main()
 {
-
-
-
-  return 0;
+ int size = 0, num = 0;
+ bool result1 = false,result2 = false;
+ cout<<"Enter the size of Array: \n";
+ cin>>size;
+ int *arr = new int[size];
+ cout<<"Enter array elements. \n";
+ for(int i=0; i<size; i++)
+     cin>>arr[i];
+ sort(arr, arr+size);
+ cout<<"Enter the element to be searched: ";
+ cin>>num;
+ result1 = binarySearchIterative(arr, 0, size-1, num);
+ result2 = binarySearchReacursive(arr, 0, size-1, num);
+ if(result1 == true)
+     cout<<num<<"Element found.\n";
+ else
+     cout<<"Element not found!\n";
+ if(result2 == true)
+     cout<<num<<"Element found.\n";
+ else
+     cout<<"Element not found!\n";
+ delete[] arr;
+ return 0;
 }
